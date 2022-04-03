e6ac7f6fd85bade87313df8d87924540cdedc50b
unknown <BigHug.yt@gmail.com>
2022-04-03 15:16:49 +0200

    added input polling

--- Benga/src/Benga/Application.cpp
+++ Benga/src/Benga/Application.cpp
@@ -5,6 +5,8 @@
 
 #include <glad/glad.h>
 
+#include "Input.h"
+
 namespace Benga {
 
 #define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
@@ -57,6 +59,10 @@ namespace Benga {
 			glClear(GL_COLOR_BUFFER_BIT);
 			for (Layer* layer : m_LayerStack)
 				layer->OnUpdate();
+
+			auto [x, y] = Input::GetMousePosition();
+			BG_CORE_TRACE("{0}, {1}", x, y);
+
 			m_Window->OnUpdate();
 		}
 	}
