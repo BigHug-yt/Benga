1c4fd60b0d6b327d796c8d15b71cad577c50817e
unknown <BigHug.yt@gmail.com>
2022-04-03 13:35:55 +0200

    added ImGui events

--- Benga/src/Benga/Events/KeyEvent.h
+++ Benga/src/Benga/Events/KeyEvent.h
@@ -52,4 +52,22 @@ namespace Benga {
 
 		EVENT_CLASS_TYPE(KeyPressed)
 	};
+
+	class BENGA_API KeyTypedEvent : public KeyEvent {
+
+	public:
+		KeyTypedEvent(int keycode)
+			: KeyEvent(keycode) {}
+
+		std::string ToString() const override {
+
+			std::stringstream ss;
+			ss << "KeyTypedEvent: " << m_KeyCode;
+			return ss.str();
+		}
+
+		EVENT_CLASS_TYPE(KeyTyped)
+	private:
+		int m_RepeatCount;
+	};
 }
--- Benga/src/Benga/ImGui/ImGuiLayer.h
+++ Benga/src/Benga/ImGui/ImGuiLayer.h
@@ -2,6 +2,10 @@
 
 #include "Benga/Layer.h"
 
+#include "Benga/Events/ApplicationEvent.h"
+#include "Benga/Events/KeyEvent.h"
+#include "Benga/Events/MouseEvent.h"
+
 namespace Benga {
 
 	class BENGA_API ImGuiLayer : public Layer {
@@ -14,6 +18,15 @@ namespace Benga {
 		void OnDetach();
 		void OnUpdate();
 		void OnEvent(Event& event);
+	private:
+		bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
+		bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
+		bool OnMouseMovedEvent(MouseMovedEvent& e);
+		bool OnMouseScrolledEvent(MouseScrolledEvent& e);
+		bool OnKeyPressedEvent(KeyPressedEvent& e);
+		bool OnKeyReleasedEvent(KeyReleasedEvent& e);
+		bool OnKeyTypedEvent(KeyTypedEvent& e);
+		bool OnWindowResizeEvent(WindowResizeEvent& e);
 	private:
 		float m_Time;
 
