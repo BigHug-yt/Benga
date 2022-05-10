35170bf5c5e928fdf49ea5f6648891d0aa7e8bde
unknown <BigHug.yt@gmail.com>
2022-05-10 19:58:03 +0200

    added Shader class (and YELLOW TRIANGLE!!!)

--- Benga/src/Benga/Application.h
+++ Benga/src/Benga/Application.h
@@ -9,6 +9,8 @@
 #include "Benga/Events/KeyEvent.h"
 #include "Benga/Events/MouseEvent.h"
 
+#include "Benga/Renderer/Shader.h"
+
 
 namespace Benga {
 
@@ -36,6 +38,7 @@ namespace Benga {
 		LayerStack m_LayerStack;
 
 		unsigned int m_VertexArray, m_VertexBuffer, m_IndexBuffer;
+		std::unique_ptr<Shader> m_Shader;
 	private:
 		static Application* s_Instance;
 	};
