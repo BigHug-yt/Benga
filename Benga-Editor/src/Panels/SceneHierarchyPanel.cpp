10cd34e3814d88cc6b103b87c44b8380204ffa7c
BigHug <bighug.yt@gmail.com>
2022-10-29 20:17:55 +0200

    Added Properties Panel

--- Benga-Editor/src/Panels/SceneHierarchyPanel.cpp
+++ Benga-Editor/src/Panels/SceneHierarchyPanel.cpp
@@ -2,6 +2,8 @@
 
 #include <imgui/imgui.h>
 
+#include <glm/gtc/type_ptr.hpp>
+
 #include "Benga/Scene/Components.h"
 
 namespace Benga {
@@ -27,13 +29,22 @@ namespace Benga {
 
 		});
 
+		if (ImGui::IsMouseDown(0) && ImGui::IsWindowHovered())
+			m_SelectionContext = {};
+
+		ImGui::End();
+
+		ImGui::Begin("Properties");
+		if (m_SelectionContext)
+			DrawComponents(m_SelectionContext);
+
 		ImGui::End();
 	}
-	
+
 	void SceneHierarchyPanel::DrawEntityNode(Entity entity) {
 
 		auto& tag = entity.GetComponent<TagComponent>().Tag;
-		
+
 		ImGuiTreeNodeFlags flags = ((m_SelectionContext == entity) ? ImGuiTreeNodeFlags_Selected : 0) | ImGuiTreeNodeFlags_OpenOnArrow;
 		bool opened = ImGui::TreeNodeEx((void*)(uint64_t)(uint32_t)entity, flags, tag.c_str());
 		if (ImGui::IsItemClicked()) {
@@ -50,6 +61,33 @@ namespace Benga {
 			ImGui::TreePop();
 		}
 
+	}
+
+	void SceneHierarchyPanel::DrawComponents(Entity entity) {
+
+		if (entity.HasComponent<TagComponent>()) {
+
+			auto& tag = entity.GetComponent<TagComponent>().Tag;
+
+			char buffer[256];
+			memset(buffer, 0, sizeof(buffer));
+			strcpy_s(buffer, sizeof(buffer), tag.c_str());
+			if (ImGui::InputText("Tag", buffer, sizeof(buffer))) {
+
+				tag = std::string(buffer);
+			}
+		}
+
+		if (entity.HasComponent<TransformComponent>()) {
+
+			if (ImGui::TreeNodeEx((void*)typeid(TransformComponent).hash_code(), ImGuiTreeNodeFlags_DefaultOpen, "Transform")) {
+
+				auto& transform = entity.GetComponent<TransformComponent>().Transform;
+				ImGui::DragFloat3("Position", glm::value_ptr(transform[3]), 0.1f);
+
+				ImGui::TreePop();
+			}
+		}
 	}
 
 }
--- Benga-Editor/src/Panels/SceneHierarchyPanel.h
+++ Benga-Editor/src/Panels/SceneHierarchyPanel.h
@@ -18,6 +18,7 @@ namespace Benga {
 		void OnImGuiRender();
 	private:
 		void DrawEntityNode(Entity entity);
+		void DrawComponents(Entity entity);
 	private:
 		Ref<Scene> m_Context;
 		Entity m_SelectionContext;
