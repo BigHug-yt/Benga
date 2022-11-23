7af15aee4f919294a3f674dd3edb5489003acb51
BigHug <bighug.yt@gmail.com>
2022-11-23 17:54:49 +0100

    Added adding and removing Entities and Components
    Bug-fixes:
    - Camera only got its viewport set on the first frame, causing a newly created camera to not show anything.

--- Benga/src/Benga/Scene/Scene.cpp
+++ Benga/src/Benga/Scene/Scene.cpp
@@ -12,7 +12,7 @@ namespace Benga {
 
 	Scene::Scene() {
 
-		
+
 	}
 
 	Scene::~Scene() {
@@ -27,6 +27,11 @@ namespace Benga {
 		auto& tag = entity.AddComponent<TagComponent>();
 		tag.Tag = name.empty() ? "Entity" : name;
 		return entity;
+	}
+
+	void Scene::DestroyEntity(Entity entity) {
+
+		m_Registry.destroy(entity);
 	}
 
 	void Scene::OnUpdate(Timestep ts) {
@@ -95,6 +100,33 @@ namespace Benga {
 			if (!cameraComponent.FixedAspectRatio)
 				cameraComponent.Camera.SetViewportSize(width, height);
 		}
+	}
+
+	template<typename T>
+	void Scene::OnComponentAdded(Entity entity, T& component) {
+		static_assert(false);
+	}
+
+	template<>
+	void Scene::OnComponentAdded<TransformComponent>(Entity entity, TransformComponent& component) {
+	}
+
+	template<>
+	void Scene::OnComponentAdded<CameraComponent>(Entity entity, CameraComponent& component) {
+
+		component.Camera.SetViewportSize(m_ViewportWidth, m_ViewportHeight);
+	}
+
+	template<>
+	void Scene::OnComponentAdded<SpriteRendererComponent>(Entity entity, SpriteRendererComponent& component) {
+	}
+
+	template<>
+	void Scene::OnComponentAdded<TagComponent>(Entity entity, TagComponent& component) {
+	}
+
+	template<>
+	void Scene::OnComponentAdded<NativeScriptComponent>(Entity entity, NativeScriptComponent& component) {
 	}
 
 }
